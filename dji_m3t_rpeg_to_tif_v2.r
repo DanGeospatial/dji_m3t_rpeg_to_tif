097dd0ef86f2342abee9b7d0c14dc27cdab8319e
DanGeospatial <87085567+DanGeospatial@users.noreply.github.com>
2024-05-15 21:38:27 -0400

    Update dji_m3t_rpeg_to_tif_v2.r
    add reflection

--- dji_m3t_rpeg_to_tif_v2.r
+++ dji_m3t_rpeg_to_tif_v2.r
@@ -36,6 +36,7 @@ run<-function(x) shell(x, intern=FALSE, wait=TRUE)
 emissivity = 0.96 # default: 1.0 range: 0.1-1.0 https://royalsocietypublishing.org/doi/pdf/10.1098/rsos.181281 (e.g., average 0.957 for vegetation)
 humidity = 38 # default: 70 %  range: 20-100 %
 distance = 25 # default: 5 m   range: 1-25 m    # altitude - object height (set to 25 if camera-target-distance > 25 m)
+reflection = 19 #default: 25 range: -50-500 This represents the temperature of the day
 
 ###  dir where your raw *.rpeg thermal images are placed. A output directory will be placed inside this folder.
 in_dir = "I:/sample_data"
@@ -63,7 +64,7 @@ foreach(i=1:length(in_files), .packages=c("hexView", "raster", "ijtiff", "exifr"
     in_name = in_files[i]
     out_name = paste0(out_dir, substr(basename(in_files[i]), 0, nchar(basename(in_files[i]))-4), ".raw")
     run(paste0("dji_irp.exe -s ", in_name, " -a measure -o ", out_name, " --measurefmt float32", 
-               " --emissivity ", emissivity, " --humidity ", humidity, " --distance ", distance))
+               " --emissivity ", emissivity, " --humidity ", humidity, " --distance ", distance, " --reflection ", reflection))
     
     # from .raw (hex) to .tif (celsius in float)
     raw_data <- readBin(out_name, "double", size = 4, n = in_exif$ImageWidth*in_exif$ImageHeight)
